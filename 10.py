7aee9c46f3bbbd78a7fbcdc894211c61073538d1
autist69 <ilyaz2008y@gmail.com>
2026-09-11 23:10:20 +0300

    'py_code'

---
+++ 10.py
@@ -0,0 +1,10 @@
+﻿with open("input.txt", "r", encoding="utf-8") as f:
+    sogl = 'йцкнгшщзхъфвпрлджчсмтьб'; gl = 'уеыаоэяиюё'
+    for line in f.readlines():
+        l = line.strip(); l_copy = ''; l_copy += l[0]
+        for i in range(1, len(l)):
+            if l[i - 1] in sogl and l[i] in gl:
+                l_copy += l[i] + 'c' + l[i]
+            else:
+                l_copy += l[i]
+        print(l_copy)
